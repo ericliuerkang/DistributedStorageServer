d6dabf22875c11c6d0e4b3295225d418d9405335
jerryzejia <jerryzejia@gmail.com>
2020-01-17 01:18:13 -0500

    Revert "Cache Interface"
    This reverts commit 2e8853d33adbc4e50ed019e5343637fdf8a24346.

--- m1-stub/src/app_kvClient/KVClient.java
+++ m1-stub/src/app_kvClient/KVClient.java
@@ -1,21 +1,11 @@
 package app_kvClient;
 
 import client.KVCommInterface;
-import java.net.Socket;
-
-import org.apache.log4j.Logger;
-
-
 
 public class KVClient implements IKVClient {
     @Override
-    private Socket clientSocket;
-    private Logger logger = Logger.getRootLogger();
-
     public void newConnection(String hostname, int port) throws Exception{
         // TODO Auto-generated method stub
-        clientSocket = new Socket(address, port);
-        logger.info("Connection Established")
     }
 
     @Override
--- m1-stub/src/cache/IKVCache.java
+++
@@ -1,7 +0,0 @@
-package cache;
-
-public interface IKVCache{
-    protected int maxCacheSize;
-    protected int currentCacheSize = 0;
-
-}
