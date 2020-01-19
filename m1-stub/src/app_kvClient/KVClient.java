11d499d89c8fa7d278731037483cee55ecc6331b
jerryzejia <jerryzejia@gmail.com>
2020-01-19 15:32:15 -0500

    Completed part of KVStore.

--- m1-stub/src/app_kvClient/KVClient.java
+++ m1-stub/src/app_kvClient/KVClient.java
@@ -1,16 +1,24 @@
 package app_kvClient;
 
 import client.KVCommInterface;
+import client.KVStore;
 
 public class KVClient implements IKVClient {
+    private KVStore kvstore = null;
+
     @Override
     public void newConnection(String hostname, int port) throws Exception{
         // TODO Auto-generated method stub
+        kvstore = new KVStore(hostname, port);
+        kvstore.connect();
     }
 
     @Override
     public KVCommInterface getStore(){
         // TODO Auto-generated method stub
-        return null;
+        return kvstore;
     }
+
+
+
 }
--- m1-stub/src/client/KVStore.java
+++ m1-stub/src/client/KVStore.java
@@ -1,5 +1,12 @@
 package client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.net.Socket;
+
+import org.apache.log4j.Logger;
 import shared.messages.KVMessage;
 
 public class KVStore implements KVCommInterface {
@@ -8,18 +15,55 @@ public class KVStore implements KVCommInterface {
 	 * @param address the address of the KVServer
 	 * @param port the port of the KVServer
 	 */
+	private Logger logger = Logger.getRootLogger();
+	private String address;
+	private int port;
+
+	private Socket clientSocket;
+	private OutputStream output;
+	private InputStream input;
+
+
 	public KVStore(String address, int port) {
 		// TODO Auto-generated method stub
+		this.port = port;
+		this.address = address;
+		// setRunning(true);
+		logger.info("Connection Established");
 	}
-
+	/**
+	 * @throws Exception
+	 */
 	@Override
 	public void connect() throws Exception {
-		// TODO Auto-generated method stub
+		clientSocket = new Socket(address, port);
+		try{
+			output = clientSocket.getOutputStream();
+			input = clientSocket.getInputStream();
+			logger.info("Connection Successful");
+		}catch (IOException ioe) {
+			logger.error("Connection could not be established!");
+		}
 	}
-
 	@Override
 	public void disconnect() {
-		// TODO Auto-generated method stub
+		logger.info("try to close connection ...");
+		try {
+			tearDownConnection();
+		} catch (Exception e) {
+			logger.error("Unable to close connection! Error:" + e);
+		}
+	}
+	private void tearDownConnection() throws IOException {
+		// setRunning(false);
+		logger.info("tearing down the connection ...");
+		if (clientSocket != null) {
+			input.close();
+			output.close();
+			clientSocket.close();
+			clientSocket = null;
+			logger.info("connection closed!");
+		}
 	}
 
 	@Override
