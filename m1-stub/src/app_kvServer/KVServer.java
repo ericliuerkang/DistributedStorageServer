ba69594937bcf8705de5e5f56c1ba94b7d3da37c
Jia  Jerry Ze <jiaze@ug176.eecg.toronto.edu>
2020-01-26 03:18:43 -0500

    reverse

--- m1-stub/src/app_kvServer/KVServer.java
+++ m1-stub/src/app_kvServer/KVServer.java
@@ -157,7 +157,7 @@ public class KVServer implements IKVServer {
 
 	@Override
     public void clearCache(){
-		if (cache!=null){
+		if (cache!=null && cache.getMaxCacheSize() != 0){
 			cache.clearCache();
 		}
 		else{
